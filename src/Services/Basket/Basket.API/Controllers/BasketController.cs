b6356812a9079434d0682201ddea63d15f629998
SPIRAL\leontyl <leontyl@spiralsolutions.com>
2021-10-28 12:43:50 +0300

    Basket service

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+
+            basket ??= new ShoppingCart(userName);
+
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            var updateResult = await _repository.UpdateBasket(basket);
+
+            return Ok(updateResult);
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
